787fd4f38fbd953fdacc00affa0ee3541cc4d6ce
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-05-22 09:33:47 -0300

    Delete 3a Introdução a JavaScript directory

--- 3a Introdução a JavaScript/exercícios/ex2/script.js
+++
@@ -1,9 +0,0 @@
-var dolar = document.getElementById("dolar")
-var real = document.getElementById("real")
-var real = document.getElementById("real")
-var calc;
-
-function converter(){
-    calc = parseInt(dolar.value) * 4.90;
-    real.innerHTML = calc;
-}
