fb24ef79ac24b3d43c99e207e32f29e1172ddc6c
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-05-24 10:20:45 -0300

    Delete 3a Introdução a JavaScript directory

--- 3a Introdução a JavaScript/exercícios/ex3/script.js
+++
@@ -1,8 +0,0 @@
-var metros = document.getElementById("metros");
-var centimetros = document.getElementById("centimetros");
-var res;
-
-function calc(){
-    res = parseInt(metros.value) * 100;
-    centimetros.innerHTML = res;
-}
