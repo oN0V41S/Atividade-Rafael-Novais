157358a31aa16e8d8fe1c226222d22b162626565
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-06-15 11:49:28 -0300

    Delete javascript directory

--- javascript/filtroCat.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById("filtro")
-const filtro = document.getElementById("catalogo")
-const itens = filtro.getElementsByTagName('div')
-
-function filter(){
-}
-console.log(itens)
