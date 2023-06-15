5be3a9b6eb9a28f3516c1f11cf26731796b50e98
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-06-15 11:48:39 -0300

    Add files via upload

---
+++ javascript/filtroCat.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("filtro")
+const filtro = document.getElementById("catalogo")
+const itens = filtro.getElementsByTagName('div')
+
+function filter(){
+}
+console.log(itens)
